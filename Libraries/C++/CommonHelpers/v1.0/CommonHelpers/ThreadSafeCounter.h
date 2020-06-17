ff726b01e74afd4eb545a4603c958e662af088bf
David Brownell <db@DavidBrownell.com>
2020-06-16 22:07:01 -0700

    Moved CV outside lock
    --HG--
    branch : ThreadPoolDeadlock

--- Libraries/C++/CommonHelpers/v1.0/CommonHelpers/ThreadSafeCounter.h
+++ Libraries/C++/CommonHelpers/v1.0/CommonHelpers/ThreadSafeCounter.h
@@ -117,10 +117,9 @@ inline ThreadSafeCounter & ThreadSafeCounter::Increment(void) {
         std::scoped_lock<decltype(_ctrMutex)>           lock(_ctrMutex); UNUSED(lock);
 
         ++_ctr;
-
-        _ctrCV.notify_all();
     }
 
+    _ctrCV.notify_all();
 #endif
 
     return *this;
@@ -137,10 +136,9 @@ inline ThreadSafeCounter & ThreadSafeCounter::Decrement(void) {
 
         assert(_ctr >= 1);
         --_ctr;
-
-        _ctrCV.notify_all();
     }
 
+    _ctrCV.notify_all();
 #endif
 
     return *this;
